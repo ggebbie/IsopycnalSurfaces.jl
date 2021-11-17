c9dde7270e4e8d06de4afad5fcb0c7ad29ba6ab9
Documenter.jl <documenter@juliadocs.github.io>
2021-11-17 20:53:26 +0000

    build based on 0307281

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SigmaShift","category":"page"},{"location":"#SigmaShift","page":"Home","title":"SigmaShift","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SigmaShift.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SigmaShift]","category":"page"}]
+}
